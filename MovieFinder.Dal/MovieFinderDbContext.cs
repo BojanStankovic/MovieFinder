e23e478ca366b97331cf4c8646fff30f51789aa7
Bojan Stankovic <bstankov1160@gmail.com>
2021-08-15 12:45:52 +0200

    Adds the ef core to the solution.

---
+++ MovieFinder.Dal/MovieFinderDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieFinder.Dal
+{
+    public class MovieFinderDbContext : DbContext
+    {
+        public MovieFinderDbContext(DbContextOptions<MovieFinderDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
