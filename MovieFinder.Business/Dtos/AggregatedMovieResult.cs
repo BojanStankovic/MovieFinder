18312c176b9d36df4638a143ea0510e12977ea10
Bojan Stankovic <bstankov1160@gmail.com>
2021-08-16 23:17:51 +0200

    Fixes several minor bugs.

--- MovieFinder.Business/Dtos/AggregatedMovieResult.cs
+++ MovieFinder.Business/Dtos/AggregatedMovieResult.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace MovieFinder.Business.Dtos
+﻿namespace MovieFinder.Business.Dtos
 {
     public class AggregatedMovieResult
     {
@@ -10,23 +8,9 @@ namespace MovieFinder.Business.Dtos
 
         public string FullTitle { get; set; }
 
-        public string Type { get; set; }
-
         public string Year { get; set; }
 
-        public string Plot { get; set; }
-
-        public string Genres { get; set; }
-
-        public string Countries { get; set; }
-
-        public string Languages { get; set; }
-
-        public string Tagline { get; set; }
-
-        public List<string> KeywordList { get; set; }
-
-        public string ErrorMessage { get; set; }
+        public string ErrorMessage { get; set; } = string.Empty;
 
         public YoutubeTrailers YoutubeTrailers { get; set; }
     }
