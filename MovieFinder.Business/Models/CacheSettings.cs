bf3edd39b6facc87650947e6d8fa86cc77a292aa
Bojan Stankovic <bstankov1160@gmail.com>
2021-08-17 01:04:31 +0200

    Adds CORS policy. Moves cache settings to appsettings.

---
+++ MovieFinder.Business/Models/CacheSettings.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace MovieFinder.Business.Models
+{
+    public class CacheSettings
+    {
+        public int AbsoluteExpirationRelativeToNowInSeconds { get; set; }
+
+        public int SizeLimit { get; set; }
+
+        public double CompactionPercentage { get; set; }
+
+        public CacheItemPriority Priority { get; set; }
+    }
+}
